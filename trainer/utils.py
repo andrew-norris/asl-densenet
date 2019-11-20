22226543dfb49d6506ea6b517b1d28de09a843ec
Andrew Norris <anorris97@gmail.com>
2019-11-20 10:59:30 -0500

    test os.listdir

--- trainer/utils.py
+++ trainer/utils.py
@@ -9,33 +9,35 @@ DATA_URL = "http://www.cvssp.org/FingerSpellingKinect2011/fingerspelling5.tar.bz
 
 def download_dataset():
 
-    tf.keras.utils.get_file(origin=DATA_URL, fname='asl_fingerspelling', extract=True)
+    os.listdir('~/.keras/datasets')
 
-    train_datagen = ImageDataGenerator()
-    valid_datagen = ImageDataGenerator()
-    test_datagen = ImageDataGenerator()
-
-    train_generator = train_datagen.flow_from_directory(
-        directory=r"~/.keras/datasets/dataset5/A",
-        target_size=(224, 224),
-        color_mode="rgb",
-        batch_size=32,
-        class_mode="categorical",
-        shuffle=True,
-        seed=42
-    )
-
-    valid_generator = valid_datagen.flow_from_directory(
-        directory=r"~/.keras/datasets/dataset5/D/",
-        target_size=(224, 224),
-        color_mode="rgb",
-        batch_size=32,
-        class_mode="categorical",
-        shuffle=True,
-        seed=42
-    )
-
-    return train_generator, valid_generator
+    # tf.keras.utils.get_file(origin=DATA_URL, fname='asl_fingerspelling', extract=True)
+    #
+    # train_datagen = ImageDataGenerator()
+    # valid_datagen = ImageDataGenerator()
+    # test_datagen = ImageDataGenerator()
+    #
+    # train_generator = train_datagen.flow_from_directory(
+    #     directory=r"~/.keras/datasets/dataset5/A",
+    #     target_size=(224, 224),
+    #     color_mode="rgb",
+    #     batch_size=32,
+    #     class_mode="categorical",
+    #     shuffle=True,
+    #     seed=42
+    # )
+    #
+    # valid_generator = valid_datagen.flow_from_directory(
+    #     directory=r"~/.keras/datasets/dataset5/D/",
+    #     target_size=(224, 224),
+    #     color_mode="rgb",
+    #     batch_size=32,
+    #     class_mode="categorical",
+    #     shuffle=True,
+    #     seed=42
+    # )
+    #
+    # return train_generator, valid_generator
 
 
 def get_next_generator(directory):
