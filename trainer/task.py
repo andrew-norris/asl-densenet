411dd77dcaad1c55ea5612f6c6740de27461c29f
Andrew Norris <anorris97@gmail.com>
2019-11-20 09:47:55 -0500

    AN - add main function.

--- trainer/task.py
+++ trainer/task.py
@@ -1,5 +1,42 @@
 import trainer.utils as utils
 from trainer.model import dense_net
+import argparse
+import tensorflow as tf
+import os
+
+def get_args():
+  """Argument parser.
+
+  Returns:
+    Dictionary of arguments.
+  """
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--job-dir',
+      type=str,
+      required=True,
+      help='local or GCS location for writing checkpoints and exporting models')
+  parser.add_argument(
+      '--num-epochs',
+      type=int,
+      default=20,
+      help='number of times to go through the data, default=20')
+  parser.add_argument(
+      '--batch-size',
+      default=128,
+      type=int,
+      help='number of records to read during each training step, default=128')
+  parser.add_argument(
+      '--learning-rate',
+      default=.01,
+      type=float,
+      help='learning rate for gradient descent, default=.01')
+  parser.add_argument(
+      '--verbosity',
+      choices=['DEBUG', 'ERROR', 'FATAL', 'INFO', 'WARN'],
+      default='INFO')
+  args, _ = parser.parse_known_args()
+  return args
 
 def train(args):
 
@@ -19,18 +56,27 @@ def train(args):
         epochs=epochs
     )
 
-    directories = ['B/', 'C/']
-    for directory in directories:
-        train_generator = utils.get_next_generator(directory)
-        model.fit_generator(
-            train_generator,
-            steps_per_epoch=batch_size,
-            valid_generator=valid_generator,
-            validation_steps=batch_size,
-            epochs=epochs
-        )
+    # directories = ['B/', 'C/']
+    # for directory in directories:
+    #     train_generator = utils.get_next_generator(directory)
+    #     model.fit_generator(
+    #         train_generator,
+    #         steps_per_epoch=batch_size,
+    #         valid_generator=valid_generator,
+    #         validation_steps=batch_size,
+    #         epochs=epochs
+    #     )
 
     model.evaluate_generator(
         valid_generator=valid_generator,
         steps=batch_size
     )
+
+    export_path = os.path.join(args.job_dir, 'keras_export')
+    tf.contrib.saved_model.save_keras_model(model, export_path)
+    print('Model exported to: {}'.format(export_path))
+
+if __name__ == '__main__':
+    args = get_args()
+    tf.logging.set_verbosity(args.verbosity)
+    train(args)
