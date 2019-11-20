5e1f561de46b8236dbbf9928e959687a50ba57fa
Andrew Norris <anorris97@gmail.com>
2019-11-20 11:20:47 -0500

    AN - test this out.

--- trainer/utils.py
+++ trainer/utils.py
@@ -8,38 +8,36 @@ DATA_URL = "http://www.cvssp.org/FingerSpellingKinect2011/fingerspelling5.tar.bz
 
 
 def download_dataset():
-    # !/usr/bin/python
 
+    tf.keras.utils.get_file(origin=DATA_URL, fname='asl_fingerspelling', extract=True)
 
-    print(os.listdir('/root/.keras'))
+    print(os.listdir('/root/.keras/datasets/dataset5'))
 
-    # tf.keras.utils.get_file(origin=DATA_URL, fname='asl_fingerspelling', extract=True)
-    #
-    # train_datagen = ImageDataGenerator()
-    # valid_datagen = ImageDataGenerator()
-    # test_datagen = ImageDataGenerator()
-    #
-    # train_generator = train_datagen.flow_from_directory(
-    #     directory=r"~/.keras/datasets/dataset5/A",
-    #     target_size=(224, 224),
-    #     color_mode="rgb",
-    #     batch_size=32,
-    #     class_mode="categorical",
-    #     shuffle=True,
-    #     seed=42
-    # )
-    #
-    # valid_generator = valid_datagen.flow_from_directory(
-    #     directory=r"~/.keras/datasets/dataset5/D/",
-    #     target_size=(224, 224),
-    #     color_mode="rgb",
-    #     batch_size=32,
-    #     class_mode="categorical",
-    #     shuffle=True,
-    #     seed=42
-    # )
-    #
-    # return train_generator, valid_generator
+    train_datagen = ImageDataGenerator()
+    valid_datagen = ImageDataGenerator()
+    test_datagen = ImageDataGenerator()
+
+    train_generator = train_datagen.flow_from_directory(
+        directory=r"/root/.keras/datasets/dataset5/A",
+        target_size=(224, 224),
+        color_mode="rgb",
+        batch_size=32,
+        class_mode="categorical",
+        shuffle=True,
+        seed=42
+    )
+
+    valid_generator = valid_datagen.flow_from_directory(
+        directory=r"/root/.keras/datasets/dataset5/D/",
+        target_size=(224, 224),
+        color_mode="rgb",
+        batch_size=32,
+        class_mode="categorical",
+        shuffle=True,
+        seed=42
+    )
+
+    return train_generator, valid_generator
 
 
 def get_next_generator(directory):
