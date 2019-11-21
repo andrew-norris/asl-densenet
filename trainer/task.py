2cef872732bab6c0f90a0f21f31fc34883f0689d
Andrew Norris <anorris97@gmail.com>
2019-11-21 01:21:29 -0500

    AN - batch size 64.

--- trainer/task.py
+++ trainer/task.py
@@ -58,10 +58,10 @@ def train(args):
     train_generator, valid_generator = utils.download_dataset()
 
     num_classes = 24
-    batch_size = 32
+    batch_size = 64
     epochs = 4
-    learning_rate = 0.01
-    decay = 0.001
+    learning_rate = 0.001
+    decay = 0.0001
     optimizer = 0
     set_size = 3
 
